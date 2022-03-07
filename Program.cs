480abc791e0182ead7442c3d49bf0600f6c6e825
Victor Hernández <victor.hhernandez27@gmail.com>
2022-03-07 15:25:07 -0300

    Adicionar arquivos de projeto.

---
+++ Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace SortearOrdenar
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Random numero = new Random();
+            int[] vetor = new int[20];
+            int aux,valor,i,j;
+
+
+
+            for (i = 0; i < vetor.Length; i++)
+            {                
+                valor = numero.Next(1, 20);
+
+                Console.WriteLine(valor);
+
+                vetor[i] = valor;   
+                
+                for(j = i; j>0; j--)
+                {
+                    if(vetor[j] < vetor[j - 1])
+                    {
+                        aux = vetor[j - 1];
+                        vetor[j - 1] = vetor[j];
+                        vetor[j] = aux;
+                    }
+                }
+            }
+            Console.WriteLine("========");
+            Console.WriteLine("Vetor");
+
+            for (i = 0; i < vetor.Length; i++)
+            {
+                Console.WriteLine(vetor[i]);
+            }
+
+        }
+    }
+}
